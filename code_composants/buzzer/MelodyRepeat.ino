d6855d853fc370baee3f1b74de9b3b79fd5ca16b
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-05-23 08:38:14 +0200

    Update MelodyRepeat.ino

--- code_composants/buzzer/MelodyRepeat.ino
+++ code_composants/buzzer/MelodyRepeat.ino
@@ -1,17 +1,3 @@
-/*
-   Created by ArduinoGetStarted.com
-
-   This example code is in the public domain
-
-   Tutorial page: https://arduinogetstarted.com/library/arduino-melody-repeat-example
-   Library References: https://arduinogetstarted.com/tutorials/arduino-buzzer-library
-
-   This example uses a piezo buzzer:
-   + play a melody on background
-   + repeat the melody when it is ended
-   + without using delay() function, this is a non-blocking example
-*/
-
 #include <ezBuzzer.h> // ezBuzzer library
 
 const int BUZZER_PIN = 3;
