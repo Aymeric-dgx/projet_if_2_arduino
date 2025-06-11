6fb14ea87dc61258d7eaaa65adad44b2231e329c
aymeric_dgx <128166467+Aymeric-dgx@users.noreply.github.com>
2025-06-11 10:26:16 +0200

    Update code_complet.ino

--- aymeric/code_complet.ino
+++ aymeric/code_complet.ino
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <DHT.h> 
 
 // Taille de l’écran (pour écran 128x64)
 #define SCREEN_WIDTH 128
@@ -26,42 +27,9 @@ int seuil_vitesse_vent = 60; //Alerte si supérieur à km/h
 int seuil_humidité = 30; // Alerte si inférieur à 30%
 int seuil_luminosité = 1000; // Seuil de luminosité pour passer de joir à nuit
 
-
-
-
-// Fonction capteur de temp
-byte dat [5];
-
-byte read_data () {
-  byte data;
-  for (int i = 0; i < 8; i ++) {
-    if (digitalRead (temp_id) == LOW) {
-      while (digitalRead (temp_id) == LOW);
-      delayMicroseconds (30);
-      if (digitalRead (temp_id) == HIGH)
-      data |= (1 << (7-i));
-      while (digitalRead (temp_id) == HIGH);
-    }
-  }
-  return data;
-}
-
-void start_test () {
-  digitalWrite (temp_id, LOW);
-  delay (30);
-  digitalWrite (temp_id, HIGH);
-  delayMicroseconds (40);
-  pinMode (temp_id, INPUT);
-  while (digitalRead (temp_id) == HIGH);
-  delayMicroseconds (80);
-  if (digitalRead (temp_id) == LOW);
-  delayMicroseconds (80);
-  for (int i = 0; i < 4; i ++)
-  dat[i] = read_data ();
-  pinMode (temp_id, OUTPUT);
-  digitalWrite (temp_id, HIGH);
-}
-
+// Création de l'objet dht pour la température
+#define DHTTYPE DHT11
+DHT dht(temp_id, DHTTYPE);
 
 
 // Loops principales
@@ -71,7 +39,6 @@ void setup () {
   pinMode (temp_id, OUTPUT);
   pinMode(ventilateur_id, OUTPUT);
 
-  // setup OLED
   // Initialiser I2C
   Wire.begin();  // SDA, SCL
 
@@ -80,27 +47,20 @@ void setup () {
     Serial.println(F("Écran OLED non détecté"));
     while (true); // Arrêt
   }
+
+  // Initialisation du capteur de temp
+  dht.begin();
 }
 
 void loop () {
   // Récupération/calcul des données
-
+  float humidté = dht.readHumidity();
+  float température = dht.readTemperature();
+  int air_value = analogRead(qualité_A0_id);
   
 
-  start_test ();
-  Serial.print ("Current humdity =");
-  Serial.print (dat [0], DEC);
-  Serial.print ('.');
-  Serial.print (dat [1], DEC);
-  Serial.println ('%');
-  Serial.print ("Current temperature =");
-  Serial.print (dat [2], DEC);
-  Serial.print ('.');
-  Serial.print (dat [3], DEC);
-  Serial.println ('C');
-
   // Récupération données qualité air
-  int air_value = analogRead(qualité_A0_id);
+  
   Serial.print("qualité air : ");
   Serial.println(air_value);
 
@@ -114,19 +74,6 @@ void loop () {
   display.setTextColor(SSD1306_WHITE); // Couleur (blanc sur fond noir)
   display.setCursor(0, 0);             // Position de départ (x, y)
 
-  // Affichage temp
-  display.print("Température :");
-  display.print (dat [2], DEC);
-  display.print ('.');
-  display.print (dat [3], DEC);
-  display.println ('C');
-
-  // Affichage humidité
-  display.print("Humidité :");
-  display.print (dat [0], DEC);
-  display.print ('.');
-  display.print (dat [1], DEC);
-  display.println ('%');
 
   // Affichage qualité humidité
   display.print(air_value);
@@ -134,15 +81,6 @@ void loop () {
 
   display.display();  // Affiche le contenu
 
-
-  // Activation/désactivation du ventilatuer
-  int actual_temp = dat[2] + dat[3]*0.1;
-  if(actual_temp > seuil_temp) {
-    digitalWrite(ventilateur_id, HIGH);
-    Serial.println("Ventilateur allumé");
-  } else {
-    digitalWrite(ventilateur_id, LOW);
-  }
 
   delay(2000);
 }
